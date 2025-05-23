12b44a406806b7dd3a5dd42285d362d71a269eaa
Samudini Gunarathna <samudinigunarathna@gmail.com>
2025-05-23 12:33:21 +0530

    Delete Day 01 directory

--- Day 01/File 03/File03.java
+++
@@ -1,9 +0,0 @@
-import java.io.*;
-
-// The first letter of a class must be capital
-class Argument2{
-	public static void main(String[] args){
-
-		System.out.println(args[0] + " " + args[2] + " " + args[1] + "?");
-	}
-}
